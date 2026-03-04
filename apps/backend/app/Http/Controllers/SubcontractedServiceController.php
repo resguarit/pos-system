bc2d22d0a4f71cea35ca82ac3b059e67629597e1
naimguar <naimguar@gmail.com>
2026-03-04 10:14:37 -0300

    refactor(transfer-schemas): simplify transfer creation schema and add refinement for branch validation

---
+++ apps/backend/app/Http/Controllers/SubcontractedServiceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SubcontractedService;
+use Illuminate\Http\Request;
+
+class SubcontractedServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SubcontractedService $subcontractedService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SubcontractedService $subcontractedService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, SubcontractedService $subcontractedService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SubcontractedService $subcontractedService)
+    {
+        //
+    }
+}
